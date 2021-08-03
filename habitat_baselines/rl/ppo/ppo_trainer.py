6b13febdca66d6c805a7e441fe98681a73ed9d45
qian <luoqian19961224@gmail.com>
2021-08-03 09:01:58 -0400

    change crop

--- habitat_baselines/rl/ppo/ppo_trainer.py
+++ habitat_baselines/rl/ppo/ppo_trainer.py
@@ -925,6 +925,8 @@ class PPOTrainer(BaseRLTrainer):
         n, c, h, w = imgs.shape
         w1 = torch.randint(0, w - size + 1, (n,))
         h1 = torch.randint(0, h - size + 1, (n,))
+        w1 = 1
+        h1 = 10
         cropped = torch.empty((n, c, size, size),dtype=imgs.dtype, device=imgs.device)
         for i, (img, w11, h11) in enumerate(zip(imgs, w1, h1)):
             cropped[i][:] = img[:, h11:h11 + size, w11:w11 + size]
